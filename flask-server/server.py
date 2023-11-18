2f56bee34764415deeb2aa4a912596546a9d9763
rrozmus <rrozmus@finitec.pl>
2023-11-18 14:32:37 +0100

    second commit

---
+++ flask-server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/test')
+def get_test():
+    return {
+        "test1": "test2"
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
