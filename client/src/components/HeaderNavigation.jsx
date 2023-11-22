d12bbfd9a5c922b530d2b80d47fac1aad312ee12
rrozmus <rrozmus@finitec.pl>
2023-11-22 14:58:45 +0100

    navigation fix

--- client/src/components/HeaderNavigation.jsx
+++ client/src/components/HeaderNavigation.jsx
@@ -3,32 +3,32 @@ import Link from "next/link";
 
 const HeaderNavigation = ({ isMenuOpen }) => {
 	const itemClass = isMenuOpen
-		? "header-navigation__item header-navigation__item--show"
-		: "header-navigation__item";
+		? "header-navigation__item header-navigation__item--in"
+		: "header-navigation__item header-navigation__item--out";
 	return (
 		<nav>
 			<ul className="header-navigation">
-				<li className={`animation-delay1 ${itemClass}`}>
+				<li className={itemClass}>
 					<Link href="/harbours" className="header-navigation__item-link">
 						Harbours
 					</Link>
 				</li>
-				<li className={`animation-delay2 ${itemClass}`}>
+				<li className={`transition-delay1 ${itemClass}`}>
 					<Link href="/boats" className="header-navigation__item-link">
 						Boats
 					</Link>
 				</li>
-				<li className={`animation-delay3 ${itemClass}`}>
+				<li className={`transition-delay2 ${itemClass}`}>
 					<Link href="/last-minute" className="header-navigation__item-link">
 						Last Minute
 					</Link>
 				</li>
-				<li className={`animation-delay4 ${itemClass}`}>
+				<li className={`transition-delay3 ${itemClass}`}>
 					<Link href="/shop" className="header-navigation__item-link">
 						Shop
 					</Link>
 				</li>
-				<li className={`animation-delay5 ${itemClass}`}>
+				<li className={`transition-delay4 ${itemClass}`}>
 					<Link href="/contact" className="header-navigation__item-link">
 						Contact
 					</Link>
