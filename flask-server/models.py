0bdc4681fd9b69ba4b4e9ff419cb235bbe9f2778
rrozmus <rrozmus@finitec.pl>
2024-03-02 20:26:49 +0100

    dependencies

--- flask-server/models.py
+++ flask-server/models.py
@@ -1,22 +1,22 @@
-from flask_sqlalchemy import SQLAlchemy
-from uuid import uuid4
+# from flask_sqlalchemy import SQLAlchemy
+# from uuid import uuid4
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
-def get_uuid():
-    return uuid4().hex
+# def get_uuid():
+#     return uuid4().hex
 
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
-    email = db.Column(db.String(345), unique=True)
-    password = db.Column(db.Text, nullable=False)
-    first_name = db.Column(db.String(100))
-    surname = db.Column(db.String(100))
+# class User(db.Model):
+#     __tablename__ = "users"
+#     id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
+#     email = db.Column(db.String(345), unique=True)
+#     password = db.Column(db.Text, nullable=False)
+#     first_name = db.Column(db.String(100))
+#     surname = db.Column(db.String(100))
     
-class Harbour (db.Model):
-    __tablename__ = "harbours"
-    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
-    name = db.Column(db.String(345))
-    localization = db.Column(db.String(345))
+# class Harbour (db.Model):
+#     __tablename__ = "harbours"
+#     id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
+#     name = db.Column(db.String(345))
+#     localization = db.Column(db.String(345))
     
