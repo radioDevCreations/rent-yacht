3ee325cb9678dd270041aeef71a3e4c81fbf4d44
radekrozmus <radoslaw.rozmus@microsoft.wsei.edu.pl>
2024-03-05 18:53:49 +0100

    add filter

--- client/src/boatify-components/BoatifySearch/BoatifySearch.tsx
+++ client/src/boatify-components/BoatifySearch/BoatifySearch.tsx
@@ -4,26 +4,25 @@ import "./BoatifySearch.scss";
 
 const BoatifySearch = () => {
 	return (
-		<>
-			<div className="input search">
-				<div className="search__label">
-					<label className="search__icon-label" htmlFor="search-input"></label>
-					<input
-						type="text"
-						id="search-input"
-						className="search__input input__field"
-						placeholder="Search..."
-					/>
-				</div>
-				<div className="search__button">
-					<BoatifyButton
-						value="Search"
-						type={ButtonType.button}
-						classModifier="boatify-button--search"
-					/>
-				</div>
+		<div className="search">
+			<div className="search__input">
+				<label className="search__input-label" htmlFor="search"></label>
+				<input
+					type="text"
+					id="search"
+					className="search__input-field input__field"
+					placeholder="Search..."
+				/>
 			</div>
-		</>
+			<div className="search__button">
+				<span className="icon icon--search"></span>
+				<BoatifyButton
+					value="Search"
+					type={ButtonType.button}
+					classModifier="boatify-button--search"
+				/>
+			</div>
+		</div>
 	);
 };
 
