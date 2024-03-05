a96660fbcfc307ba1c12c28b033d86d79ae00911
rrozmus <rrozmus@finitec.pl>
2024-03-05 10:16:57 +0100

    build type filter

--- client/src/boatify-components/BoatifySearch/BoatifySearch.tsx
+++ client/src/boatify-components/BoatifySearch/BoatifySearch.tsx
@@ -6,21 +6,22 @@ const BoatifySearch = () => {
 	return (
 		<>
 			<div className="input search">
-				<label className="search__icon-label" htmlFor="search-input"></label>
-				<input
-					type="text"
-					id="search-input"
-					className="search__input input__field"
-					placeholder="Search..."
-				/>
-				<button type="submit" className="search__button">
-					<span className="icon icon--search"></span>
+				<div className="search__label">
+					<label className="search__icon-label" htmlFor="search-input"></label>
+					<input
+						type="text"
+						id="search-input"
+						className="search__input input__field"
+						placeholder="Search..."
+					/>
+				</div>
+				<div className="search__button">
 					<BoatifyButton
 						value="Search"
 						type={ButtonType.button}
 						classModifier="boatify-button--search"
 					/>
-				</button>
+				</div>
 			</div>
 		</>
 	);
--- client/src/components/BoatsFilter/BoatsFilter.tsx
+++ client/src/components/BoatsFilter/BoatsFilter.tsx
@@ -1,5 +1,31 @@
+import "./BoatsFilter.scss";
+import { FaSailboat } from "react-icons/fa6";
+import { IoMdBoat } from "react-icons/io";
+
 const BoatsFilter = () => {
-	return <section className="filter">filter</section>;
+	return (
+		<section className="filter">
+			<div className="filter__section">
+				<div className="filter__type-option">
+					<span>ALL</span>
+				</div>
+				<div className="filter__type-option">
+					<span className="filter__type-icon">
+						<FaSailboat />
+					</span>
+				</div>
+				<div className="filter__type-option">
+					<span className="filter__type-icon">
+						<IoMdBoat />
+					</span>
+				</div>
+			</div>
+			<div className="filter__section"></div>
+			<div className="filter__section"></div>
+			<div className="filter__section"></div>
+			<div className="filter__section"></div>
+		</section>
+	);
 };
 
 export default BoatsFilter;
