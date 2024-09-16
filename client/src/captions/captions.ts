5373a2e3bacbf98af8d5daf46c5cb9261eb04cd7
radioDevCreations <radekrozmus@gmail.com>
2024-09-16 19:48:50 +0200

    add captions file

---
+++ client/src/captions/captions.ts
@@ -0,0 +1,8 @@
+enum Captions {
+	APP_NAME = "Boatify",
+	PAGE_NAME_HARBOURS = "Harbours",
+	PAGE_NAME_BOATS = "Boats",
+	PAGE_NAME_LAST_MINUTE = "Last Minute",
+	PAGE_NAME_SHOP = "Shop",
+	PAGE_NAME_CONTACT = "Contact",
+}
