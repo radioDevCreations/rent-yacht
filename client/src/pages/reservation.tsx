92ed53ea839cc3ef371f6d15700add76801852b5
radioDevCreations <radekrozmus@gmail.com>
2024-10-06 11:28:59 +0200

    add reservation components

---
+++ client/src/pages/reservation.tsx
@@ -0,0 +1,13 @@
+import "./last-minute.scss";
+import MainLayout from "@/layouts/MainLayout/MainLayout";
+import Reservation from "@/components/Reservation/Reservation";
+
+const ReservationPage = () => {
+	return (
+		<MainLayout>
+			<Reservation />
+		</MainLayout>
+	);
+};
+
+export default ReservationPage;
